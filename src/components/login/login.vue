b093ca77e5abb17e848730bcf942fe377a53cf8c
lzw <2532109293@qq.com>
2020-02-10 19:24:54 +0800

    引用提示框

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,11 +1,61 @@
 <template>
-    <div>
-        denglu
-    </div>
+<div class="login-wrap">
+    <el-form label-position="top" label-width="80px" :model="formdata" class="login-form">
+        <h2>用户登录</h2>
+        <el-form-item label="用户名">
+            <el-input v-model="formdata.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+            <el-input v-model="formdata.password"></el-input>
+        </el-form-item>
+        <el-button type="primary" class="login-btn" @click.prevent="">登录</el-button>
+    </el-form>
+</div>
 </template>
 
 <script>
-export default { }
+export default {
+  data () {
+    return {
+      formdata: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    handLogin () {
+      this.$http.post('login', this.formdata).then(res => {
+        const {
+          // eslint-disable-next-line no-unused-vars
+          data, meta: {msg, staus}
+        } = res.data
+        if (staus === 200) {
+          this.$router.push({name: 'home'})
+          this.$message.success(msg)
+        } else {
+          this.$message.warning(msg)
+        }
+      })
+    }
+  }
+}
 </script>
 
-<style></style>
+<style>
+.login-wrap {
+    height: 100%;
+    background-color: #324152;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.login-wrap .login-form {
+    width: 400px;background-color: #fff;
+    border-radius: 5px;
+    padding: 30px;
+}
+.login-wrap .login-btn {
+    width: 100%;
+}
+</style>
